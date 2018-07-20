5cb0b80b039107c850f53a978ff66756bf9f9a03
ttsing <yzz@ttsing.com>
2018-07-20 12:02:18 +0800

    增加gateway后端异常全局提示

--- src/www/src/api/index.js
+++ src/www/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-
+import { Message } from 'element-ui'
 
 const HTTP = axios.create({
   // 基础url前缀
@@ -11,6 +11,48 @@ const HTTP = axios.create({
 
   }
 });
+
+let message
+
+
+HTTP.interceptors.request.use(config => {
+  return config
+}, (error) => {
+  return Promise.reject(error)
+})
+
+// response拦截器
+HTTP.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    if (error && error.response) {
+      switch (error.response.status) {
+        case 400:
+          break
+        case 401:
+          break
+        case 403:
+          return
+        case 404:
+          break
+        case 408:
+          break
+        case 500:
+          break
+        case 504:
+          break
+        default:
+          error.message = '未知错误'
+      }
+      message = Message.error(error.message)
+    } else {
+      message = Message.error(error.message)
+    }
+    return {error: -1, data: ''}
+  }
+)
 
 export function Clusters(data) {
   return HTTP({
